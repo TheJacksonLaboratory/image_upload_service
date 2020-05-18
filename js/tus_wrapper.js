8701ca4b388f362d88a36550fe183ba7ce71bf99
Josh Marshall <josh.marshall@jax.org>
2020-05-18 14:42:02 -0400

    Pushing web page changes.

--- js/tus_wrapper.js
+++ js/tus_wrapper.js
@@ -1,17 +1,49 @@
 #!/usr/bin/env node
-// TODO: Get the javascript people and env dev, maybe even a few distros, 
-// add `js` as a shell option like sh.
-
 
 var tus = require("tus-js-client");
 
-function null_case(){
-	console.log("Do functions even work?")
+//const mutex = new Mutex();
+
+// https://stackoverflow.com/a/9050354
+if (!Array.prototype.last){
+  Array.prototype.last = function(){
+    return this[this.length - 1];
+  };
+};
+
+
+function refresh_pending_and_completed_lists(pending_uploads, completed_uploads){
+
+  string_list_pending = [];
+  string_list_completed = [];
+
+  pending_uploads.forEach(e => string_list_pending.push(e.name));
+  completed_uploads.forEach(e => string_list_completed.push(e.name));
+
+  if(string_list_pending.length > 0){
+    document.getElementById("pending_files").innerHTML = "Pending: " + string_list_pending.join(", ") + "<br>";
+  }else{
+    document.getElementById("pending_files").innerHTML = "";
+  }
+
+  if(string_list_completed.length > 0){
+    document.getElementById("uploaded_files").innerHTML = "Completed: " + string_list_completed.join(", ");
+  }else{
+    document.getElementById("uploaded_files").innerHTML = "";
+  }
 }
 
-null_case();
 
-function tus_wrapper(file, filename, filesize, email, password, lab, project, instrument){ 
+function tus_wrapper(pending_uploads, completed_uploads, email, lab, project, instrument){ 
+
+    document.getElementById("upload_text").innerHTML = ""; 
+    document.getElementById("upload_progress").style.display = "none"; 
+    refresh_pending_and_completed_lists(pending_uploads, completed_uploads); 
+
+    if(!email || !lab || !project || !instrument || pending_uploads.length <= 0){ 
+	    document.getElementById("upload_bar").style.display = "none"; 
+	    return; 
+    }
 
     var tus_options = {
         endpoint: "http://10.4.72.133:1080/files/",
@@ -19,7 +51,7 @@ function tus_wrapper(file, filename, filesize, email, password, lab, project, in
             "Upload-Metadata": JSON.stringify(
                 {
                     "JAX_EMAIL": btoa(email),
-                    "JAX_PASSWORD": btoa(password),
+                    //"JAX_PASSWORD": btoa(password),
                     "JAX_LAB": btoa(lab),
                     "JAX_INSTRUMENT": btoa(instrument),
                     "JAX_PROJECT": btoa(project)
@@ -27,7 +59,7 @@ function tus_wrapper(file, filename, filesize, email, password, lab, project, in
             )
 		},
 		metadata: {
-			filename: filename,
+			filename: pending_uploads.last().name,
 			filetype: "application/octet-stream"
 		},
 		onError: function(error) {
@@ -40,21 +72,55 @@ function tus_wrapper(file, filename, filesize, email, password, lab, project, in
 			document.getElementById("upload_progress").innerHTML = bytesUploaded + "/" + bytesTotal;
 		},
 		onSuccess: function() {
-			console.log("Download %s from %s", filename, upload.url);
+			console.log("Download %s from %s", pending_uploads.last().name, upload.url);
+			completed_uploads.push(pending_uploads.pop());
+			document.getElementById("upload_text").innerHTML = "";
+			document.getElementById("upload_progress").style.display = "none";
+                        tus_wrapper(pending_uploads, completed_uploads, email, password, lab, project, instrument);
 		},
 		//uploadLengthDeferred: true,
 		resume: true,
 		//removeFingerprintOnSuccess: true,
-		uploadSize: filesize,
+		uploadSize: pending_uploads.last().size,
 		chunkSize: 5 * 1024 * 1024
 	};
 
-	document.getElementById("upload_text").innerHTML = filename;
-	var upload = new tus.Upload(file, tus_options);
+	document.getElementById("upload_bar").style.display = "block";
+	document.getElementById("upload_text").innerHTML = pending_uploads.last().name;
+	document.getElementById("upload_progress").style.display = "block";
+	var upload = new tus.Upload(pending_uploads.last(), tus_options);
 
 	upload.start();
 }
 
-null_case();
+function verifyAndUpload(){
+  console.log("in central function");
+  // Get form data
 
-module.exports = {null_case: null_case, tus_wrapper: tus_wrapper};
+  let email_val = document.getElementById("email").value;
+  //let password_val = document.getElementById("password").value;
+  let lab_val = document.getElementById("lab").value;
+  let project_val = document.getElementById("project").value;
+  let instrument_val = document.getElementById("instrument").value;
+  let image_file = document.getElementById("image");
+
+  pending_uploads = []
+  completed_uploads = []
+
+  for(var i=0; i < image_file.files.length; ++i){
+    pending_uploads.push(image_file.files[i]);
+  }
+
+
+  tus_wrapper(
+    pending_uploads,
+    completed_uploads,
+    email_val,
+    lab_val,
+    project_val,
+    instrument_val
+  );
+}
+
+module.exports = {tus_wrapper: tus_wrapper, verifyAndUpload: verifyAndUpload};
+
